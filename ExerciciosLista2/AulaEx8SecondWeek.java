ff222a1cc52422e8143ec4e14207bf0c24e6894e
thgttm <100978258+thgttm@users.noreply.github.com>
2022-08-08 17:52:12 -0300

    Update AulaEx8SecondWeek.java

--- ExerciciosLista2/AulaEx8SecondWeek.java
+++ ExerciciosLista2/AulaEx8SecondWeek.java
@@ -4,30 +4,39 @@ import java.util.Scanner;
 
 public class AulaEx8SecondWeek {
     public static void main(String[] args) {
-        Scanner ler = new Scanner(System.in);
-        int n[] = new int[5];
-        int aux;
-        for (int i = 0; i < n.length; i++) {
-            System.out.println("Escreva o " + (i + 1) + "º número.");
-            n[i] = ler.nextInt();
-        }
-
-
-        for (int i = 0; i < n.length; i++) {
-            for (int j = (i + 1); j < n.length; j++) {
-                if (n[j] < n[i]) {
-                    aux = n[i];
-                    n[i] = n[j];
-                    n[j] = aux;
+        Scanner sc = new Scanner(System.in);
+        int[] numero = new int[5];
+        int i = 0, x, aux;
+        do {
+            System.out.println("Insira o " + (i + 1) + "º número:");
+            numero[i] = sc.nextInt();
+            for (x = 0; x < 5; x++) {
+                if (numero[i] == numero[x] && i != x) {
+                    System.out.println("Este número já está cadastrado");
+                    i--;
+                    break;
                 }
             }
-        }
-        for (int i = 0; i < n.length; i++) {
-            if (i == 4) {
-                System.out.println(n[i] + ".");
-            } else {
-                System.out.print(n[i]+" | ");
+            i++;
+        } while (i < 5);
+
+        do {
+            x = 0;
+            for (i = 0; i <= 3; i++) {
+                if (numero[i] > numero[i + 1]) {
+                    aux = numero[i];
+                    numero[i] = numero[i + 1];
+                    numero[i + 1] = aux;
+                    x = 1;
+                }
             }
+        } while (x != 0);
+        System.out.println("A ordem crescente dos números é ");
+        for (i = 0; i <= 3; i++) {
+            System.out.print(numero[i] + ", ");
         }
+        System.out.println(numero[4] + ".");
     }
 }
+
+    
